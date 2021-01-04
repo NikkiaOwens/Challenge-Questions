b865749834589cac705d4d7f0da7010d9ad8ec36
nikki <nikki@Gbrads-Mac.local>
2021-01-04 14:02:39 -0500

    Jewels and Stones

--- src/Test/HowManyVowelsTest.java
+++ src/Test/HowManyVowelsTest.java
@@ -5,8 +5,15 @@ public class HowManyVowelsTest {
 
     HowManyVowels howManyVowels;
 
+    /**
+     * When a string is inputted, should return how many vowels are in said string
+     */
     @Test
     public void TestHowManyVowels(){
-        
+        //Given
+
+        //When
+
+        //Then
     }
 }
